627cc0a815efa1b02e10fdd5c06e9ee16abad2a3
harrison7 <harrison@thecaspers.net>
2024-03-19 19:37:31 -0600

    More implementation

--- client/src/main/java/serverFacade/ServerFacade.java
+++ client/src/main/java/serverFacade/ServerFacade.java
@@ -3,22 +3,21 @@ package serverFacade;
 import chess.*;
 import dataAccess.DataAccessException;
 import model.*;
+import ui.State;
 
 import java.util.UUID;
 
 public class ServerFacade {
     private int port;
     private AuthData authToken;
+    private State state;
     public ServerFacade(int port) {
         this.port = port;
+        state = State.PRELOGIN;
     }
 
-    public AuthData register(UserData user) throws DataAccessException {
-        if (user.password() == null) {
-            throw new DataAccessException("Null password");
-        } else {
-            return new AuthData(UUID.randomUUID().toString(), user.username());
-        }
+    public String register(UserData user) throws DataAccessException {
+
     }
 
 
--- client/src/main/java/ui/PreloginUI.java
+++ client/src/main/java/ui/PreloginUI.java
@@ -1,4 +1,21 @@
 package ui;
 
+import dataAccess.DataAccessException;
+import model.UserData;
+import serverFacade.ServerFacade;
+
 public class PreloginUI {
+
+    private State state;
+
+    public String register(UserData user) throws DataAccessException {
+        if (!user.password().isEmpty()) {
+            //state = State.POSTLOGIN;
+            visitorName = String.join("-", params);
+            ws = new ServerFacade(serverUrl, notificationHandler);
+            ws.enterPetShop(visitorName);
+            return String.format("You signed in as %s.", visitorName);
+        }
+        throw new DataAccessException("Null password");
+    }
 }
