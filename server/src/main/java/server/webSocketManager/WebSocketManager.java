cb375e40f2a4837882d906eb81954db4830ae46d
harrison7 <harrison@thecaspers.net>
2024-04-09 20:09:04 -0600

    Actually I'll do the petshop technique

--- server/src/main/java/server/webSocketManager/WebSocketManager.java
+++ server/src/main/java/server/webSocketManager/WebSocketManager.java
@@ -1,4 +1,10 @@
 package server.webSocketManager;
+import org.eclipse.jetty.websocket.api.*;
 
 public class WebSocketManager {
+    public WebSocketManager() {}
+
+    public void evaluateMessage(Session session, String message) {
+
+    }
 }
